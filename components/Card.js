d51969e847b25cf08fb9784af3462d9a83cd966b
Hossein Krouna <krouna.hossein@gmx.net>
2023-01-21 07:17:15 +0100

    align items in the list

--- components/Card.js
+++ components/Card.js
@@ -8,15 +8,15 @@ function Card() {
     <>
       <StyledWrapper>
         {cards.map(({ id, start_time, end_time, location }) => (
-          <li key={id}>
+          <StyledList key={id}>
             {
-              <div>
+              <StyledDiv>
                 {start_time}
                 {end_time}
                 {location}
-              </div>
+              </StyledDiv>
             }
-          </li>
+          </StyledList>
         ))}
       </StyledWrapper>
     </>
@@ -25,9 +25,24 @@ function Card() {
 
 export default Card;
 
+const StyledDiv = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const StyledList = styled.li`
+  display: flex;
+  border: 1px solid;
+  border-radius: 8px;
+  /* justify-content: space-between; */
+`;
 const StyledWrapper = styled.ul`
   display: flex;
   flex-direction: column;
   margin: 10px;
   gap: 50px;
+  list-style: none;
+  padding: 0;
+  border: 1px solid;
+  border-radius: 8px;
 `;
