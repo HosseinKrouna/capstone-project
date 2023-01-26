9f2fda40a85fe00fd54ff761ac1b75326466be63
Hossein Krouna <krouna.hossein@gmx.net>
2023-01-26 15:29:34 +0100

    add toggle buttons

--- pages/events/[id].js
+++ pages/events/[id].js
@@ -1,8 +1,15 @@
 import { useRouter } from "next/router";
 import events from "@/db.json";
+import styled from "styled-components";
 import Link from "next/link";
+import { useState } from "react";
 
 function EventDetail() {
+  // const [isThumpUp, setIsThumpUp] = useState(false);
+  // const [maybe, setMaybe] = useState("");
+  // const [thumpDown, setThumpDown] = useState("");
+  const [emojiCheckmark, setEmojiCheckmark] = useState("");
+
   const router = useRouter();
   const { id } = router.query;
 
@@ -12,20 +19,103 @@ function EventDetail() {
     return <h1>hmm... no event yet 🤔?</h1>;
   }
 
-  const { title, startTime, endTime, location, creator, isParticipant } =
-    currentEvent;
+  const { title, startTime, endTime, location, creator } = currentEvent;
 
+  // function onToggleCheckmarks() {
+  //   }
+  function thumpUpHandler() {
+    if (!emojiCheckmark) {
+      setEmojiCheckmark("👍");
+    } else {
+      setEmojiCheckmark("");
+    }
+  }
+
+  function maybeHandler() {
+    if (!emojiCheckmark) {
+      setEmojiCheckmark("❓");
+    } else {
+      setEmojiCheckmark("");
+    }
+  }
+
+  function thumpDownHandler() {
+    if (!emojiCheckmark) {
+      setEmojiCheckmark("👎");
+    } else {
+      setEmojiCheckmark("");
+    }
+  }
   return (
     <>
-      <h2>{title}</h2>
-      <span>{startTime}</span>
-      <span>{endTime}</span>
-      <p>Location: {location}</p>
-      <p>Creator: {creator}</p>
-      <p>Are you there? {isParticipant ? "Yes" : "No"}</p>
-      <Link href="/events">Go Back</Link>
+      <StyledWrapper>
+        <h2>{title}</h2>
+        <span>{`${emojiCheckmark}`}</span>
+        <StyledCardDiv>
+          <span>{startTime}</span>
+          <span>{endTime}</span>
+          <p>Location: {location}</p>
+          <p>Creator: {creator}</p>
+        </StyledCardDiv>
+        <StyledButtonDiv>
+          <StyledButtons
+            type="button"
+            // thumpUp={isThumpUp}
+            onClick={thumpUpHandler}
+          >
+            👍
+          </StyledButtons>
+          <StyledButtons type="button" onClick={maybeHandler}>
+            ❓
+          </StyledButtons>
+          <StyledButtons type="button" onClick={thumpDownHandler}>
+            👎
+          </StyledButtons>
+        </StyledButtonDiv>
+        <Link href="/events">
+          <StyledLink>↩︎</StyledLink>
+        </Link>
+      </StyledWrapper>
     </>
   );
 }
+
+const StyledButtons = styled.button`
+  font-size: 35px;
+`;
+
+const StyledWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0 auto 12px auto;
+`;
+
+const StyledLink = styled.a`
+  display: flex;
+  background-color: lightblue;
+  border: 1px solid;
+  padding: 10px;
+  margin: 50px 170px;
+  font-size: 25px;
+  border-radius: 10px;
+`;
+
+const StyledButtonDiv = styled.div`
+  display: flex;
+  justify-content: space-around;
+
+  gap: 10px;
+`;
+
+const StyledCardDiv = styled.div`
+  display: flex;
+  margin: 10px;
+  padding: 10px;
+  flex-direction: column;
+  border: 3px solid;
+  background-color: limegreen;
+  border-radius: 5px;
+`;
 
 export default EventDetail;
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import events from "@/db.json";
+import styled from "styled-components";
 
 function EventsPage() {
   return (
@@ -8,12 +9,24 @@ function EventsPage() {
       <ul>
         {events.map((event) => (
           <li key={event.id}>
-            <Link href={`/events/${event.id}`}>{event.title}</Link>
+            <StyledCardDiv>
+              <Link href={`/events/${event.id}`}>{event.title}</Link>
+            </StyledCardDiv>
           </li>
         ))}
       </ul>
     </>
   );
 }
+
+const StyledCardDiv = styled.div`
+  display: flex;
+  margin: 10px;
+  padding: 10px;
+  flex-direction: column;
+  border: 3px solid;
+  background-color: limegreen;
+  border-radius: 5px;
+`;
 
 export default EventsPage;
