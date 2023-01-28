c8992245890b87f2e1afd7bd548a50e9b932f006
Hossein Krouna <krouna.hossein@gmx.net>
2023-01-28 14:09:04 +0100

    add: fieldset

--- components/CreateEventForm.js
+++ components/CreateEventForm.js
@@ -1,7 +1,12 @@
 import styled from "styled-components";
 
-function CreateEventForm({ formName, defaultData }) {
-  function handleSubmit() {}
+function CreateEventForm({ onSubmit, formName, defaultData }) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+    onSubmit(data);
+  }
 
   return (
     <>
@@ -11,70 +16,85 @@ function CreateEventForm({ formName, defaultData }) {
         </header>
         <main>
           <FormContainer aria-labelledby={formName} onSubmit={handleSubmit}>
-            <label htmlFor="title">Title:</label>
-            <input
-              id="title"
-              name="title"
-              type="text"
-              defaultValue={defaultData?.title}
-            />
-            <label htmlFor="title">Start Time:</label>
-            <input
-              id="start-time"
-              name="Start-time"
-              type="date"
-              defaultValue={defaultData?.startTime}
-            />
-            <label htmlFor="title">End Time:</label>
-            <input
-              id="end-time"
-              name="end-time"
-              type="date"
-              defaultValue={defaultData?.endTime}
-            />
-            <label htmlFor="title">Location:</label>
-            <input
-              id="location"
-              name="location"
-              type="text"
-              defaultValue={defaultData?.location}
-            />
-            <label htmlFor="title">Map URL:</label>
+            <fieldset>
+              <FormItems>
+                <legend>Create a new event 👇</legend>
+                <label htmlFor="title">Title:</label>
+                <input
+                  id="title"
+                  name="title"
+                  type="text"
+                  defaultValue={defaultData?.title}
+                />
+                <label htmlFor="start-time">Start Time:</label>
+                <input
+                  id="start-time"
+                  name="startTime"
+                  type="datetime-local"
+                  defaultValue={defaultData?.startTime}
+                />
+                <label htmlFor="end-time">End Time:</label>
+                <input
+                  id="end-time"
+                  name="endTime"
+                  type="datetime-local"
+                  defaultValue={defaultData?.endTime}
+                />
+                <label htmlFor="location">Location:</label>
+                <input
+                  id="location"
+                  name="location"
+                  type="text"
+                  defaultValue={defaultData?.location}
+                />
+                <label htmlFor="map-url">Map URL:</label>
 
-            <input
-              id="map-url"
-              name="mapURL"
-              type="text"
-              defaultValue={defaultData?.mapURL}
-            />
+                <input
+                  id="map-url"
+                  name="mapURL"
+                  type="text"
+                  defaultValue={defaultData?.mapURL}
+                />
 
-            <label htmlFor="title">Good To Know:</label>
-            <input
-              id="description"
-              name="description"
-              type="text"
-              defaultValue={defaultData?.description}
-            />
-            <label htmlFor="title">Creator:</label>
-            <input
-              id="creator"
-              name="creator"
-              type="text"
-              defaultValue={defaultData?.creator}
-            />
-            <button type="submit">
-              {defaultData ? "Update Event" : "Add Event"}
-            </button>
+                <label htmlFor="description">Good To Know:</label>
+                <textarea
+                  cols="30"
+                  id="description"
+                  name="description"
+                  type="text"
+                  defaultValue={defaultData?.description}
+                />
+                <label htmlFor="title">Creator:</label>
+                <input
+                  id="creator"
+                  name="creator"
+                  type="text"
+                  defaultValue={defaultData?.creator}
+                />
+                <button type="submit">
+                  {defaultData ? "Update Event" : "Add Event"}
+                </button>
+              </FormItems>
+            </fieldset>
           </FormContainer>
         </main>
       </StyledWrapper>
     </>
   );
 }
+const FormItems = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
 const FormContainer = styled.form`
   display: flex;
   flex-direction: column;
   gap: 5px;
+  align-items: center;
+  justify-content: start;
+  height: 100vh;
+  font-family: cursive;
 `;
 
 const StyledWrapper = styled.div`
