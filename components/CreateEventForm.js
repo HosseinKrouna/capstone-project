22d64b3da5cf93674b14def09300c406a7f46303
Hossein Krouna <krouna.hossein@gmx.net>
2023-01-28 13:03:46 +0100

    add: basic styles an one more inputfield for mapURL

--- components/CreateEventForm.js
+++ components/CreateEventForm.js
@@ -1,60 +1,87 @@
+import styled from "styled-components";
+
 function CreateEventForm({ formName, defaultData }) {
   function handleSubmit() {}
 
   return (
     <>
-      <header>
-        <h1>New Event</h1>
-      </header>
-      <main>
-        <form aria-labelledby={formName} onSubmit={handleSubmit}>
-          <label htmlFor="title">Title:</label>
-          <input
-            id="title"
-            name="title"
-            type="text"
-            defaultValue={defaultData?.title}
-          />
-          <label htmlFor="title">Start Time:</label>
-          <input
-            id="startTime"
-            name="StartTime"
-            type="text"
-            defaultValue={defaultData?.startTime}
-          />
-          <label htmlFor="title">End Time:</label>
-          <input
-            id="endTime"
-            name="endTime"
-            type="text"
-            defaultValue={defaultData?.endTime}
-          />
-          <label htmlFor="title">Location:</label>
-          <input
-            id="location"
-            name="location"
-            type="text"
-            defaultValue={defaultData?.location}
-          />
-          <label htmlFor="title">Good To Know:</label>
-          <input
-            id="description"
-            name="description"
-            type="text"
-            defaultValue={defaultData?.description}
-          />
-          <label htmlFor="title">Creator:</label>
-          <input
-            id="creator"
-            name="creator"
-            type="text"
-            defaultValue={defaultData?.creator}
-          />
-          <button type="submit">Submit</button>
-        </form>
-      </main>
+      <StyledWrapper>
+        <header>
+          <h1>New Event</h1>
+        </header>
+        <main>
+          <FormContainer aria-labelledby={formName} onSubmit={handleSubmit}>
+            <label htmlFor="title">Title:</label>
+            <input
+              id="title"
+              name="title"
+              type="text"
+              defaultValue={defaultData?.title}
+            />
+            <label htmlFor="title">Start Time:</label>
+            <input
+              id="start-time"
+              name="Start-time"
+              type="date"
+              defaultValue={defaultData?.startTime}
+            />
+            <label htmlFor="title">End Time:</label>
+            <input
+              id="end-time"
+              name="end-time"
+              type="date"
+              defaultValue={defaultData?.endTime}
+            />
+            <label htmlFor="title">Location:</label>
+            <input
+              id="location"
+              name="location"
+              type="text"
+              defaultValue={defaultData?.location}
+            />
+            <label htmlFor="title">Map URL:</label>
+
+            <input
+              id="map-url"
+              name="mapURL"
+              type="text"
+              defaultValue={defaultData?.mapURL}
+            />
+
+            <label htmlFor="title">Good To Know:</label>
+            <input
+              id="description"
+              name="description"
+              type="text"
+              defaultValue={defaultData?.description}
+            />
+            <label htmlFor="title">Creator:</label>
+            <input
+              id="creator"
+              name="creator"
+              type="text"
+              defaultValue={defaultData?.creator}
+            />
+            <button type="submit">
+              {defaultData ? "Update Event" : "Add Event"}
+            </button>
+          </FormContainer>
+        </main>
+      </StyledWrapper>
     </>
   );
 }
+const FormContainer = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+`;
+
+const StyledWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0 auto 12px auto;
+`;
 
 export default CreateEventForm;
