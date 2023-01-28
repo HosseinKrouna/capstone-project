0ca101c4775e1fcb2a4c6e0ecf77c47c2139d76c
Hossein Krouna <krouna.hossein@gmx.net>
2023-01-28 12:31:53 +0100

    add: input fields

--- components/CreateEventForm.js
+++ components/CreateEventForm.js
@@ -1,12 +1,5 @@
-import { useState } from "react";
-
-function CreateEventForm() {
-  const [title, setTitle] = useState("");
-  const [startTime, setStartTime] = useState("");
-  const [EndTime, setEndTime] = useState("");
-  const [location, setLocation] = useState("");
-  const [information, setInformation] = useState("");
-  const [creator, setCreator] = useState("");
+function CreateEventForm({ formName, defaultData }) {
+  function handleSubmit() {}
 
   return (
     <>
@@ -14,7 +7,51 @@ function CreateEventForm() {
         <h1>New Event</h1>
       </header>
       <main>
-        <form action=""></form>
+        <form aria-labelledby={formName} onSubmit={handleSubmit}>
+          <label htmlFor="title">Title:</label>
+          <input
+            id="title"
+            name="title"
+            type="text"
+            defaultValue={defaultData?.title}
+          />
+          <label htmlFor="title">Start Time:</label>
+          <input
+            id="startTime"
+            name="StartTime"
+            type="text"
+            defaultValue={defaultData?.startTime}
+          />
+          <label htmlFor="title">End Time:</label>
+          <input
+            id="endTime"
+            name="endTime"
+            type="text"
+            defaultValue={defaultData?.endTime}
+          />
+          <label htmlFor="title">Location:</label>
+          <input
+            id="location"
+            name="location"
+            type="text"
+            defaultValue={defaultData?.location}
+          />
+          <label htmlFor="title">Good To Know:</label>
+          <input
+            id="description"
+            name="description"
+            type="text"
+            defaultValue={defaultData?.description}
+          />
+          <label htmlFor="title">Creator:</label>
+          <input
+            id="creator"
+            name="creator"
+            type="text"
+            defaultValue={defaultData?.creator}
+          />
+          <button type="submit">Submit</button>
+        </form>
       </main>
     </>
   );
