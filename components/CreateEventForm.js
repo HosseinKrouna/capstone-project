288e51cb80aa5d42ba82bbbcebf84d18925a19b9
Hossein Krouna <krouna.hossein@gmx.net>
2023-01-28 11:38:37 +0100

    add: useStates for CreateFormPage

--- components/CreateEventForm.js
+++ components/CreateEventForm.js
@@ -1,5 +1,23 @@
+import { useState } from "react";
+
 function CreateEventForm() {
-  return <div>create</div>;
+  const [title, setTitle] = useState("");
+  const [startTime, setStartTime] = useState("");
+  const [EndTime, setEndTime] = useState("");
+  const [location, setLocation] = useState("");
+  const [information, setInformation] = useState("");
+  const [creator, setCreator] = useState("");
+
+  return (
+    <>
+      <header>
+        <h1>New Event</h1>
+      </header>
+      <main>
+        <form action=""></form>
+      </main>
+    </>
+  );
 }
 
 export default CreateEventForm;
