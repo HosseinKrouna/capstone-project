8c433aa6daa4509696893da5e8ead0fc826a451c
hosseink <117902132+HosseinKrouna@users.noreply.github.com>
2023-06-05 09:14:20 +0200

    fix: fetch cloudinary via vercel

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -14,28 +14,24 @@ cloudinary.config({
 });
 
 export default async function handler(request, response) {
-	switch (request.method) {
-		case "POST":
-			const files = await parseAsync(request);
-			const { imageFile } = files;
-			const result = await cloudinary.v2.uploader.upload(imageFile.filepath, {
-				public_id: imageFile.newFilename,
-			});
+	if (request.method === "POST") {
+		const files = await parseAsync(request);
+		const { imageFile } = files;
+		const result = await cloudinary.v2.uploader.upload(imageFile.filepath, {
+			public_id: imageFile.newFilename,
+		});
+		console.log(result);
+		response.status(201).json(result);
 
-			// const result = await cloudinary.v2.uploader
-			// 	.upload(imageFile.filepath)
-			// 	.then((uploadResult) =>
-			// 		console.log(JSON.stringify(uploadResult, null, 2))
-			// 	)
-			// 	.catch((error) => console.error("error: ", error));
-
-			console.log(result);
-			// response.status(405).json({ message: "failed to load data" });
-			response.status(201).json(result);
-			break;
-		default:
-			response.status(405).json({ error: "Method Not Allowed" });
-			response.status(400).json({ message: "Bad Request" });
+		// const result = await cloudinary.v2.uploader
+		// 	.upload(imageFile.filepath)
+		// 	.then((uploadResult) =>
+		// 		console.log(JSON.stringify(uploadResult, null, 2))
+		// 	)
+		// 	.catch((error) => console.error("error: ", error));
+	} else {
+		// response.status(405).json({ message: "failed to load data" });
+		response.status(405).json({ error: "Method Not Allowed" });
 	}
 }
 
